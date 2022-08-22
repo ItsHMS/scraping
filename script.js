f2a7debeaa26e8aa8c9a2ac72dc47e2da6a598df
itsHMS <hamidmehmood816@gmail.com>
2022-08-22 12:13:38 +0500

    puppeteer

--- script.js
+++ script.js
@@ -4,22 +4,31 @@ console.log("runnig");
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://vuetifyjs.com/en/');
+    // await page.goto('https://vuetifyjs.com/en/');
+    
+    await page.goto('https://vuetifyjs.com/en/about/sponsors-and-backers/#cursory');
     await page.waitForNetworkIdle({waitUntil:"networkidle0"})
 
     await page.screenshot({path: 'screenshot.png'});
     const data = await page.evaluate(() => {
       return {
-        html: document.documentElement.innerHTML,
+        html: document.documentElement.innerText,
         width: document.documentElement.clientWidth,
         height: document.documentElement.clientHeight,
       };
     });
   
   
-
-    const $ = cheerio.load(data.html);
-    const ele = $(".v-btn__content");
-    console.log(ele.text());
+    const dat = data.html
+    
+    console.log(typeof dat)
+    // const $ = cheerio.load(data.html);
+    // const ele = $(".v-btn__content");
+    // console.log(ele.text());
+    function extractEmails ( text ){
+      return text.match(/([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9_-]+)/gi);
+      }
+       
+      console.log(extractEmails(dat))
     await browser.close();
   })();
