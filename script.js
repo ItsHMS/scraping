5fd89903fa8eca65e19ea53b8c8122eb52e34ef9
itsHMS <hamidmehmood816@gmail.com>
2022-08-22 12:16:03 +0500

    simple puppeteer

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
-console.log("runnig");
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    // await page.goto('https://vuetifyjs.com/en/');
     
     await page.goto('https://vuetifyjs.com/en/about/sponsors-and-backers/#cursory');
     await page.waitForNetworkIdle({waitUntil:"networkidle0"})
@@ -12,23 +9,19 @@ console.log("runnig");
     await page.screenshot({path: 'screenshot.png'});
     const data = await page.evaluate(() => {
       return {
-        html: document.documentElement.innerText,
+        text: document.documentElement.innerText,
         width: document.documentElement.clientWidth,
         height: document.documentElement.clientHeight,
       };
     });
   
   
-    const dat = data.html
-    
-    console.log(typeof dat)
-    // const $ = cheerio.load(data.html);
-    // const ele = $(".v-btn__content");
-    // console.log(ele.text());
+    const text = data.html
+
     function extractEmails ( text ){
       return text.match(/([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9_-]+)/gi);
       }
        
-      console.log(extractEmails(dat))
+      console.log(extractEmails(text))
     await browser.close();
   })();
